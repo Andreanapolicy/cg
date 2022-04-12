406b8b8f1c84efcb183f85e4d97e0313de60a0b0
Andreanapolicy <daa.and@mail.ru>
2022-04-12 20:36:51 +0300

    Начал выполнение 1 лабы. Разбираюсь с открытием окон

---
+++ lab1/Initials/Initials/Initials.cpp
@@ -0,0 +1,12 @@
+﻿#include <tchar.h>
+#include <windows.h>
+
+int WINAPI WinMain(
+	HINSTANCE /*hInstance*/,
+	HINSTANCE /*hPrevInstance*/,
+	LPSTR /*lpCmdLine*/,
+	int /*nCmdShow*/)
+{
+	MessageBox(NULL, _T("Hello world"), _T("My first windows program"), MB_OK | MB_ICONINFORMATION);
+	return 0;
+}

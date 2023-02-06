e532b21dc3bb1f7e8bf94b4292301bccb35eb4c8
Andreanapolicy <daa.and@mail.ru>
2023-02-06 20:33:37 +0300

    Lab1, Task 3. Инициализировал проект

---
+++ lab1/Circle/Circle/Circle.h
@@ -0,0 +1,32 @@
+
+// Circle.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CCircleApp:
+// See Circle.cpp for the implementation of this class
+//
+
+class CCircleApp : public CWinApp
+{
+public:
+	CCircleApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CCircleApp theApp;

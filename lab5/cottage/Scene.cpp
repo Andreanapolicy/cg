7578dcff0e0d2456a1baa8adc23564c82db9ff57
Andreanapolicy <daa.and@mail.ru>
2023-04-15 20:21:11 +0300

    Lab5. Добавил немного травы

--- lab5/cottage/Scene.cpp
+++ lab5/cottage/Scene.cpp
@@ -39,13 +39,21 @@ void Scene::DrawLand() const
 	auto floor = CreateCube(1);
 
 	InitLandTextureIfNeeded();
+	InitGrassTextureIfNeeded();
 	glEnable(GL_TEXTURE_2D);
-	m_landTexture.Bind();
-
+	
 	for (auto indexI = -2.0f; indexI <= 2.0f; indexI += 1.0f)
 	{
 		for (auto indexJ = -2.0f; indexJ <= 2.0f; indexJ += 1.0f)
 		{
+			if ((int)indexI + (int)indexJ % 2 == 0)
+			{
+				m_landTexture.Bind();		
+			}
+			else
+			{
+				m_grassTexture.Bind();			
+			}
 			glPushMatrix();
 			{
 				glScalef(4.0f, 0.1f, 4.0f);
--- lab5/cottage/TextureResourceName.h
+++ lab5/cottage/TextureResourceName.h
@@ -9,5 +9,5 @@ const std::wstring LAND_TEXTURE = L"./texture/land_texture.jpg";
 const std::wstring GLASS_TEXTURE = L"./texture/window_texture.png";
 const std::wstring DOOR_TEXTURE = L"./texture/door_texture.png";
 const std::wstring STEEL_TEXTURE = L"./texture/steel_texture.png";
-const std::wstring GRASS_TEXTURE = L"./texture/grass_texture.png";
+const std::wstring GRASS_TEXTURE = L"./texture/grass_texture.jpg";
 } // namespace texture::name
